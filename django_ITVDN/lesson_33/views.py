c7574bce5f116228e20ce6d1963bc78af6dea727
Granzberg <gering225@gmail.com>
2021-06-18 14:02:29 +0300

    'lesson_2'

--- django_ITVDN/lesson_33/views.py
+++ django_ITVDN/lesson_33/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def index(request):
     lets_do_it = [{'priority': 100, 'task': 'Составить список дел'},
                   {'priority': 150, 'task': 'Изучать Django'},
                   {'priority': 1, 'task': 'Подумать о смысле жизни'}]
-    return render(request, 'index.html', context=lets_do_it)
+    for _ in lets_do_it:
+        return HttpResponse(request, 'index.html')
--- mysite/polls/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-
-def hello(request):
-    print("Hi!")
-    return HttpResponse('Hello, world!')
