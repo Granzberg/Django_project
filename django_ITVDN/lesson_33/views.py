61c3d5a279078fe823cc55bbc03e9dda29c97b53
Granzberg <gering225@gmail.com>
2021-06-23 10:06:29 +0300

    'update_lesson_3'

--- django_ITVDN/lesson_33/views.py
+++ django_ITVDN/lesson_33/views.py
@@ -6,5 +6,5 @@ def index(request):
     lets_do_it = [{'priority': 100, 'task': 'Составить список дел'},
                   {'priority': 150, 'task': 'Изучать Django'},
                   {'priority': 1, 'task': 'Подумать о смысле жизни'}]
-    for _ in lets_do_it:
-        return HttpResponse(request, 'index.html')
+    for table in lets_do_it:
+        return HttpResponse(request, 'index.html',{table})
