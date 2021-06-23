7e8cba3ee109c9212d63ced658cc0e12b954c388
Granzberg <gering225@gmail.com>
2021-06-23 13:18:24 +0300

    'update'

--- django_ITVDN/lesson_33/urls.py
+++ django_ITVDN/lesson_33/urls.py
@@ -4,4 +4,8 @@ from lesson_33 import views
 
 urlpatterns = [
     path('index/', views.index),
+    path('start/', views.start),
+    path('start/lei/', views.lei),
+    path('start/luke/', views.luke),
+    path('start/khan/', views.khan),
 ]
--- django_ITVDN/lesson_33/views.py
+++ django_ITVDN/lesson_33/views.py
@@ -9,3 +9,24 @@ def index(request):
                   {'priority': 1, 'task': 'Подумать о смысле жизни'}]
     response = JsonResponse(lets_do_it, safe=False)
     return response
+
+
+def start(request):
+    return HttpResponse(request, 'start.html')
+
+
+def luke(request):
+    return HttpResponse("Страница Люка: "
+                        "Люк Скайуокер — один из главных персонажей вселенной «Звёздных войн»,"
+                        " джедай, сын сенатора с Набу Падме Амидалы Наберри и рыцаря-джедая Энакина Скайуокера")
+
+
+def lei(request):
+    return HttpResponse("Страница Леи: "
+                        "Лея Органа — дочь рыцаря-джедая Энакина Скайуокера и сенатора Падме Амидалы Наберри.")
+
+
+def khan(request):
+    return HttpResponse("Страница Хана: "
+                        "Хан. Соло — пилот космического корабля «Тысячелетний сокол»,"
+                        " его бортмехаником и вторым пилотом является вуки по имени Чубакка.")
