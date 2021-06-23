02d055c6908a2c3c204cba57d0ac03a506ecf2a7
Granzberg <gering225@gmail.com>
2021-06-23 11:49:16 +0300

    'update'

--- django_ITVDN/lesson_33/views.py
+++ django_ITVDN/lesson_33/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 
 def index(request):
     lets_do_it = [{'priority': 100, 'task': 'Составить список дел'},
                   {'priority': 150, 'task': 'Изучать Django'},
                   {'priority': 1, 'task': 'Подумать о смысле жизни'}]
-    for table in lets_do_it:
-        return HttpResponse(request, 'index.html',{table})
+    response = JsonResponse(lets_do_it, safe=False)
+    return HttpResponse(request, 'index.html', content=response)
