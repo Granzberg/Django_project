d3ab09b1c8696c8480d893a5adfdd470840033bc
Granzberg <gering225@gmail.com>
2021-06-24 11:34:13 +0300

    'lesson_3_task_2'

--- django_ITVDN/lesson_33/urls.py
+++ django_ITVDN/lesson_33/urls.py
@@ -5,7 +5,7 @@ from lesson_33 import views
 urlpatterns = [
     path('index/', views.index),
     path('start/', views.start),
-    path('start/lei/', views.lei),
-    path('start/luke/', views.luke),
-    path('start/khan/', views.khan),
+    path('start/lei/', views.lei, name='lei'),
+    path('start/luke/', views.luke, name='luke'),
+    path('start/khan/', views.khan, name='khan'),
 ]
