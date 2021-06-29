f7ae587b15bd33adea2c62b985d9c0dbccc3ddac
Granzberg <gering225@gmail.com>
2021-06-29 14:24:50 +0300

    'Lesson_5_additional_task'

--- django_ITVDN/lesson_55/admin.py
+++ django_ITVDN/lesson_55/admin.py
@@ -5,3 +5,7 @@ from . import models
 admin.site.register(models.Сartridge)
 admin.site.register(models.Basket)
 admin.site.register(models.Client)
+
+admin.site.register(models.ClientInternet)
+admin.site.register(models.Goods)
+admin.site.register(models.Category)
--- django_ITVDN/lesson_55/models.py
+++ django_ITVDN/lesson_55/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class Сartridge(models.Model):
@@ -22,3 +22,42 @@ class Basket(models.Model):
     help_text = 'Use with care and discretion.'
     count = models.IntegerField(default=1, null=True)
     cartridge = models.ManyToManyField(Сartridge)
+
+# ******************** internet shop ******************************************
+class Category(models.Model):
+    name = models.CharField(max_length=50, default=False)
+    description = models.TextField(max_length=200, default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Goods(models.Model):
+    name = models.CharField(max_length=65, blank=True)
+    image = models.ImageField(blank=True)
+    url_wiki = models.URLField(default='https://uk.wikipedia.org/', blank=True)
+    description = models.TextField(max_length=1000, blank=True)
+    available = models.BooleanField(default=False)
+    count = models.IntegerField(default=0, blank=True)
+    when_available = models.DateTimeField(default=datetime.now, blank=True)
+    discount_available = models.DurationField(default=timedelta(days=7), blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=8, blank=True)
+    discount  = models.FloatField(default=1.0, blank=True)
+    link = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class ClientInternet(models.Model):
+    first_name = models.CharField(max_length=50, blank=True)
+    surname = models.CharField(max_length=50, blank=True)
+    user_uuid = models.UUIDField(editable=False)
+    email = models.EmailField(blank=True)
+    activation_date = models.DateField(auto_now=False, blank=True)
+    ip = models.GenericIPAddressField(blank=True, null=True, protocol='IPv4')
+    invoice = models.FileField(blank=True)
+    many_link = models.ManyToManyField(Goods, verbose_name='You ordered these goods.', blank=True)
+
+    def __str__(self):
+        return self.first_name
