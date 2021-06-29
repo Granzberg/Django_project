f9bc7e8808edf329f18baf47cf1a5294843c9c77
Granzberg <gering225@gmail.com>
2021-06-29 09:26:45 +0300

    'Lesson_5_task_3'

--- django_ITVDN/lesson_55/admin.py
+++ django_ITVDN/lesson_55/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+admin.site.register(models.Сartridge)
+admin.site.register(models.Basket)
+admin.site.register(models.Client)
